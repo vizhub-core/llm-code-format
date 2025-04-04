e50ffe618678db950414fbd73a10ae724ec0b434
Curran <curran.kelleher@gmail.com>
2025-04-04 19:01:19 -0400

    Attempt to fix NPM dist

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-export { parseMarkdownFiles } from "./parseMarkdownFiles.js";
-export { serializeMarkdownFiles } from "./serializeMarkdownFiles.js";
+export { parseMarkdownFiles } from "./parseMarkdownFiles";
+export { serializeMarkdownFiles } from "./serializeMarkdownFiles";
 export {
   StreamingMarkdownParser,
   StreamingParserCallbacks,
-} from "./streamingParser.js";
+} from "./streamingParser";
